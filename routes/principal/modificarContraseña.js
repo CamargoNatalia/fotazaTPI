dd4f75d5d6fdfdd596c6a274633b533f4ca17c57
Camargo <camargonatalia83@gmail.com>
2026-06-03 09:59:03 -0300

    styles, denuncias y comentarios completos

---
+++ routes/principal/modificarContraseña.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const modiControllers = require('../controllers/usuarios/modificarControllers');
+const router = express.Router();
+
+router.get('/', modiControllers.formulario);
+router.post('/', modiControllers.modificar);
+
+module.exports = router;
---
+++ routes/principal/recuperar.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const recuperarControllers = require('../controllers/usuarios/recuperarController');
+
+const router = express.Router();
+
+router.get('/', recuperarControllers.formulario);
+router.post('/', recuperarControllers.recuperar);
+module.exports = router;
