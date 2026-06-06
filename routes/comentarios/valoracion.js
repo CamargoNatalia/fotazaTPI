c9660d0b975330f7d4c482b44bce11e8c4028ae0
Camargo <camargonatalia83@gmail.com>
2026-06-05 21:44:45 -0300

    mvc de valoraciones

---
+++ routes/comentarios/valoracion.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const valoracionControllers = require('../controllers/comentarios/valoracionControllers');
+const { requireAuth } = require('../middleware/auth');
+
+
+router.post('/:archivoId', requireAuth, valoracionControllers.guardar);
+
+module.exports = router;
