1a837ad0b7bcbd21a9f27bcd1d3b9bd36a9aeb4e
Camargo <camargonatalia83@gmail.com>
2026-06-08 17:31:24 -0300

    mvc para compra de imagen

---
+++ routes/publicacion/moderacion.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const moderacionControllers = require('../controllers/publicaciones/moderacionControllers');
+const { requireAuth, requireAdmin} = require('../middleware/auth');
+
+
+router.get('/', requireAuth, requireAdmin, moderacionControllers.listar);
+router.post('/:publicacionId', requireAuth, requireAdmin, moderacionControllers.veredicto);
+
+module.exports = router;
